8b200186f2090f47685fd514e7069f29e238e57c
Frederik Petersen <frederikpetersen@Frederiks-MacBook-Pro-6.local>
2020-12-08 11:53:42 +0100

    script v1

---
+++ script.js
@@ -0,0 +1,26 @@
+let content;
+let container = document.querySelector("#container");
+let temp = document.querySelector("template");
+
+window.addEventListener("load", sidenVises);
+
+function sidenVises() {
+    console.log("sidenVises");
+
+    document.querySelector(".menuknap").addEventListener("click", toggleMenu);
+
+}
+
+const link = "http://vais.dk/kea/02_SEM/eksamen/wordpress/wp-json/wp/v2/pages/" + id;
+
+document.addEventListener("DOMContentLoaded", hentData);
+
+
+async function hentData() {
+    const respons = await fetch(link);
+    content = await respons.json();
+    console.log(content.title.rendered);
+    console.log(content.content.rendered);
+    document.querySelector("section").innerHTML = content.content.rendered;
+
+}
